6097297d89629ca5e5d352e7d1bcbc74abaecf77
PEDRO HENRIQUE DE SOUSA GOMES <25203650@CFP127ALUNO.LOCAL>
2026-03-09 10:53:39 -0300

    Atividade 1 Finalizada

---
+++ 01-teste-aptidao/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function avaliar (){
+    const tempo = document.getElementById('tempo')
+    const resultado = document.getElementById('resultado')
+
+    if(tempo.value <14){
+        resultado.textContent = 'Apto'
+        removerClasses()
+        resultado.classList.add('apto')
+    }else{
+        resultado.textContent = 'Não Apto'
+        removerClasses()
+        resultado.classList.add('nao-apto') 
+    }
+}
+
+function removerClasses(){
+    const resultado = document.getElementById('resultado')
+    resultado.classList.remove('apto','nao-apto')
+}
